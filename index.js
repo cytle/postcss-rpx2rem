226f85660bc19060c38ee8e66a27a4bef98ea8b0
炒饭 <chaofan@2dfire.com>
2018-07-13 19:41:00 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-var postcss = require('postcss');
+const postcss = require('postcss');
+const objectAssign = require('object-assign');
 
-module.exports = postcss.plugin('postcss-rpx2rem', function (opts) {
-    opts = opts || {};
+const defaults = {
+    targetUnit: 'rpx',
+    outputUnit: 'rem',
+    proportion: 10 / 750, // 10rem / 750rpx; proportion = ouput / target
+    unitPrecision: 5,
+    replace: true,
+    mediaQuery: false
+};
 
-    // Work with options here
+function toFixed(number, precision) {
+    const multiplier = Math.pow(10, precision + 1);
+    const wholeNumber = Math.floor(number * multiplier);
+    return (Math.round(wholeNumber / 10) * 10) / multiplier;
+}
 
-    return function (root, result) {
+function declarationExists(decls, prop, value) {
+    return decls.some(decl => decl.prop === prop && decl.value === value);
+}
 
-        // Transform CSS AST here
+module.exports = postcss.plugin('postcss-rpx2rem', (options) => {
+    const {
+        targetUnit,
+        outputUnit,
+        proportion,
+        unitPrecision,
+        replace,
+        mediaQuery
+    } = objectAssign({}, defaults, options);
 
+    const replaceFn = (m, $1) => {
+        if (!$1) {
+            return m;
+        }
+        const value = toFixed(parseFloat($1) * proportion, unitPrecision);
+        return value === 0 ? '0' : `${value}${outputUnit}`;
+    };
+    const replaceRegex = new RegExp(`(\\d*\\.?\\d+)${targetUnit}`);
+
+    return function rpx2rem(root, result) {
+        root.walkRules((rule) => {
+            rule.walkDecls((decl) => {
+                if (decl.value.indexOf(targetUnit) === -1) return;
+                const value = decl.value.replace(replaceRegex, replaceFn);
+                // if rem unit already exists, do not add or replace
+                if (declarationExists(decl.parent, decl.prop, value)) return;
+                if (replace) {
+                    decl.value = value;
+                } else {
+                    decl.parent.insertAfter(result, decl.clone({ value }));
+                }
+            });
+        // We'll put more code here in a moment…
+        });
+
+        if (mediaQuery) {
+            root.walkAtRules('media', (rule) => {
+                if (rule.value.indexOf(targetUnit) === -1) return;
+                rule.params = rule.params.replace(replaceRegex, replaceFn);
+            });
+        }
     };
 });
